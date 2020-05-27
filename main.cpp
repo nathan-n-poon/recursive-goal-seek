cc1eab5620dd1f81400fbd89a9820b77db81c059
Nathan Poon <nathannpoon@gmail.com>
2020-05-27 06:56:02 +0000

    metrics for real

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <time.h>
 #define finePrecision 0.000001
 #define broadPrecision 0.01
 #define fineSensitivity 2.0*finePrecision
@@ -8,6 +9,14 @@
 #define power 4
 using namespace std;
 
+int iterations = 0;
+
+
+bool equivalent(double result, double sensitivity)
+{
+  return abs(result - (double)goal) < sensitivity;
+}
+
 double POverA(double interest)
 {
   return (pow((1+interest),power)-1.0) / (interest*pow((1.0+interest),power));
@@ -15,33 +24,78 @@ double POverA(double interest)
 
 double result(double arg, double (*f)(double))
 {
-  return (*f)(arg);
+  //return (*f)(arg);
+  return POverA(arg);
 }
 
-double average(double bound, int count)
-{ 
-  double difference = result(bound, POverA) - (double)goal;
-  if (abs(difference) < fineSensitivity)
+double interest(bool positive, double bound)
+{
+  double sum = 0.0;
+  int count = 0;
+  bool found = false;
+  if (positive)
   {
-    cout << "took " << count << " recursions" << endl;
-    return bound;
-  }
-  if(difference < 0)
-  {
-    return average(bound - bound / pow(2.0,count), count + 1);
+    for( double i = bound; i > 0.0; i-=finePrecision)
+    {
+      iterations ++;
+      if (abs(result(i, POverA) - goal) < fineSensitivity)
+      {
+        sum += i;
+        count++;
+        found = true;
+      }
+      else if(found)
+      {
+        break;
+      }
+    }
   }
 
-  return average(bound + bound / pow(2.0,count), count + 1);
-  
+  else
+  {
+    for( double i = bound; i < 1.0; i+=finePrecision)
+    {
+      iterations ++;
+      if (abs(result(i, POverA) - goal) < fineSensitivity)
+      {
+        sum += i;
+        count++;
+        found = true;
+      }
+      else if(found)
+      {
+        break;
+      }
+    }
+  }
+
+  return (double)((double)sum/(double)count);
 }
 
 int main() {
   clock_t tStart = clock();
   double avg = 999;
 
-  avg = average(0.5 , 1);
+  for (double j = 0.0; j < 1.0; j+=broadPrecision)
+  {
+    iterations++;
+    double difference = result(j, POverA) - (double)goal;
+    if(abs(difference) < broadSensitivity)
+    {
+      if(difference < 0)
+      {
+        avg = interest(true, j);
+      }
+      else 
+      {
+        avg = interest(false, j);
+      }
+      break;
+    }
+  }
 
   cout << "took " << (double)(clock() - tStart)/CLOCKS_PER_SEC << " seconds to complete" << endl;
+  cout << "took " << iterations << " iterations" << endl;
   cout << "calculated interest: " << avg << endl;
   cout << "result: " << result(avg, POverA) << endl;
   cout << "goal: " << goal << endl;
